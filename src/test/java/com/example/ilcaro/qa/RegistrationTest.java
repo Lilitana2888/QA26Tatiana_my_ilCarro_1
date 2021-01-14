4ccb611630a6092086dc2988c91554cb0f9381e1
Lilitana2888 <76058199+Lilitana2888@users.noreply.github.com>
2021-01-14 13:36:26 +0200

    fill signup form

--- src/test/java/com/example/ilcaro/qa/RegistrationTest.java
+++ src/test/java/com/example/ilcaro/qa/RegistrationTest.java
@@ -8,29 +8,68 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.sound.midi.MidiSystem;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class RegistrationTest {
     WebDriver wd;
 
     @BeforeMethod
-    public void setUp() throws InterruptedException {
+    public void setUp()  {
         wd = new ChromeDriver();
         wd.get("https://ilcarro-dev-v1.firebaseapp.com/");
-       // Thread.sleep(1000);
+
     }
 
     @Test
     public void setRegistration()
     {
-        WebElement element = wd.findElement(By.name("address"));
-        element.click();
-        element.sendKeys("Tel Aviv");
+        //open reg form click to sign up
+        wd.findElement(By.cssSelector("[href=\'/signup\']")).click();
+
+        //fill reg form
+        wd.findElement(By.cssSelector("#first_name[ name='first_name']")).click();
+        wd.findElement(By.cssSelector("#first_name[ name='first_name']")).clear();
+        wd.findElement(By.cssSelector("#first_name[ name='first_name']")).sendKeys("Tatiana");
+
+        wd.findElement(By.cssSelector("#second_name[name*='second_name']")).click();
+        wd.findElement(By.cssSelector("#second_name[name*='second_name'] ")).clear();
+        wd.findElement(By.cssSelector("#second_name[name*='second_name'] ")).sendKeys("Gessen");
+
+        wd.findElement(By.id("email")).click();
+        wd.findElement(By.id("email")).clear();
+       wd.findElement(By.id("email")).sendKeys("kupidon1971@gmail.com");
+
+        wd.findElement(By.id("password")).click();
+       wd.findElement(By.id("password")).clear();
+       wd.findElement(By.id("password")).sendKeys("Abc12345");
+
+        wd.findElement(By.id("check_policy")).click();
+        WebElement btnYalla = wd.findElement(By.cssSelector(".yalla_yalla__1Jxk6"));
+       Boolean yallabtn = btnYalla.isEnabled();
+       if(yallabtn = true)
+       {
+           btnYalla.click();
+       }
+       else
+           wd.manage().timeouts().implicitlyWait(2000, TimeUnit.MICROSECONDS);
+             btnYalla.click();
+
+
+
+//       List<WebElement>  list = wd.findElements(By.className("signup__fields"));
+//       int listsize = list.size();
+//       System.out.println(listsize);
+
 
     }
 
 
     @AfterMethod
-    public void tearDown()
-    {
+    public void tearDown() throws InterruptedException {
+         Thread.sleep(4000);
+      //  wd.manage().timeouts().implicitlyWait(4000, TimeUnit.MICROSECONDS);don't work
         wd.quit();
     }
 }
